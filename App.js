c3197975c717372a094f1c9fb9d97c3d23e69250
maryanne <mary.jereh@gmail.com>
2019-05-29 17:59:37 +0300

    added the configuration object to my app

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {GiftedChat } from 'react-native-gifted-chat';
+import {dialogflowConfig} from "./env";
 
 export default class App extends React.Component {
   state = {
     messages:[
       {
         _id:1,
-        text:"hello Developer",
+        text:"Hello am  a Faq bot, how can help you.. ",
         user:{
           _id: 2,
           name:'Help Desk',
@@ -17,10 +18,20 @@ export default class App extends React.Component {
       }
     ]
   };
+  onSend(messages = []){
+    this.setState(previousState =>({
+      messages: GiftedChat.append(previousState.messages,messages)
+    }))
+  }
   render() {
     return (
       <View style={styles.container}>
-        <GiftedChat messages={this.state.messages} />
+        <GiftedChat messages={this.state.messages}
+                   onSend={messages =>this.onSend(messages)}
+                   user={{
+                     _id:1
+                   }}
+        />
       </View>
     );
   }
