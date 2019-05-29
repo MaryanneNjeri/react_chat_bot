b74494a60a5731171427abea8c10e0088a23d119
maryanne <mary.jereh@gmail.com>
2019-05-29 17:14:02 +0300

    added gifted chat

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {GiftedChat } from 'react-native-gifted-chat';
 
 export default class App extends React.Component {
+  state = {
+    messages:[
+      {
+        _id:1,
+        text:"hello Developer",
+        user:{
+          _id: 2,
+          name:'Help Desk',
+          avatar:'https://img.icons8.com/bubbles/100/000000/lol-female.png'
+        },
+        createdAt: new Date()
+      }
+    ]
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <GiftedChat messages={this.state.messages} />
       </View>
     );
   }
@@ -14,8 +29,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
